68879255568d8ea0286432e8575044893438a83c
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2025-04-14 13:11:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def generate_caption_api():
     except Exception as e:
         return jsonify({"error": str(e), "success": False})
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
