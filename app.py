a2d5adc29343fe53b2d6321f69702e911638db9c
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2025-04-14 14:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,6 +93,6 @@ def home():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 9090))
     app.run(host="0.0.0.0", port=port)
 
