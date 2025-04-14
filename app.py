4c7130092066ddcc4d76cdcd2d2ea297c01ff013
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2025-04-14 14:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,10 @@ def generate_caption_api():
     except Exception as e:
         return jsonify({"error": str(e), "success": False})
 
+@app.route("/")
+def home():
+    return "Hello, World!"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
